928bb54000874a8a4e7b113f165b74c4258038c9
s-mdhaffar <s.mdhaffar@gmail.com>
2022-10-26 21:09:29 +0100

    register page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,30 @@
+import './login.scss';
+
+const Login = () => {
+	return (
+		<div className="login">
+			<div className="card">
+				<div className="left">
+					<h1>Hello World.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores veritatis molestiae perferendis
+						maiores magnam vel tempore vitae. Totam aspernatur aut ratione eveniet quasi ipsa voluptatem
+						debitis autem quae! Eaque, vero!
+					</p>
+					<span>Don't you have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className="right">
+					<h1>Login</h1>
+					<form>
+						<input type="text" placeholder="Username" />
+						<input type="password" placeholder="Password" />
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
