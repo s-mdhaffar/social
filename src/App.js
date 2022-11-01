6fb3a5879b17b934fd500b317939ebb0b0c0e937
s-mdhaffar <s.mdhaffar@gmail.com>
2022-11-01 22:50:07 +0000

    Ourtlet setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, Outlet, Navigate } from 'react-router-dom';
 import NavBar from './components/navBar/NavBar';
 import LeftBar from './components/leftBar/LeftBar';
 import RightBar from './components/rightBar/RightBar';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 
-const Layout = () => {
-	return (
-		<div>
-			<NavBar />
-			<div style={{ display: 'flex' }}>
-				<LeftBar />
-				<Outlet />
-				<RightBar />
-			</div>
-		</div>
-	);
-};
-
 function App () {
+	const currentUser = true;
+
+	const Layout = () => {
+		return (
+			<div>
+				<NavBar />
+				<div style={{ display: 'flex' }}>
+					<LeftBar />
+					<Outlet />
+					<RightBar />
+				</div>
+			</div>
+		);
+	};
+
+	const ProtectedRoute = ({ children }) => {
+		if (!currentUser) {
+			return <Navigate to="/login" />;
+		}
+		return children;
+	};
+
 	const router = createBrowserRouter([
 		{
 			path     : '/',
-			element  : <Layout />,
+			element  : (
+				<ProtectedRoute>
+					<Layout />
+				</ProtectedRoute>
+			),
 			children : [
 				{
 					path    : '/',
