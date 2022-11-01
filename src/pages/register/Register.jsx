26523134328809cf6bd799114e4c6aab959f1d3d
s-mdhaffar <s.mdhaffar@gmail.com>
2022-11-01 15:38:02 +0000

    Login page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,30 @@
 import './register.scss';
 
 const Register = () => {
-	return <div className="register">Register</div>;
+	return (
+		<div className="register">
+			<div className="card">
+				<div className="right">
+					<h1>Hello World.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores veritatis molestiae perferendis
+						maiores magnam vel tempore vitae. Totam aspernatur aut ratione eveniet quasi ipsa voluptatem
+						debitis autem quae! Eaque, vero!
+					</p>
+					<span>Don't you have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className="left">
+					<h1>Login</h1>
+					<form>
+						<input type="text" placeholder="Username" />
+						<input type="password" placeholder="Password" />
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
